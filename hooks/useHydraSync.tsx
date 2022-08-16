7006661b79b5696a3f8fbaf581dcb0123c5fd69d
Dean Wallflower <dean@deanwallflower.com>
2022-08-16 12:39:55 +0200

    make reusable canvas comp and hook for ticker

---
+++ hooks/useHydraSync.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+class HydraSync {
+  static window: Window;
+  static singleton: HydraSync | boolean = false;
+
+  startTimestamp: any;
+  prevTimestamp: any;
+  hydraInstances: any[];
+
+  static init(window: Window) {
+    HydraSync.window = window;
+  }
+
+  constructor() {
+    if (!HydraSync.window) {
+      throw new Error(
+        "HydraSync must be configured before creating an instance"
+      );
+    }
+    if (!HydraSync.singleton) {
+      HydraSync.singleton = true;
+      HydraSync.singleton = new HydraSync();
+      const step = HydraSync.singleton.step;
+      HydraSync.window.requestAnimationFrame(step);
+    }
+    this.hydraInstances = [];
+    return HydraSync.singleton as HydraSync;
+  }
+  step(timestamp: any) {
+    if (this.startTimestamp === undefined) {
+      this.startTimestamp = timestamp;
+      this.prevTimestamp = timestamp;
+    }
+    const dt = timestamp - this.prevTimestamp;
+
+    this.hydraInstances.forEach((instance) => {
+      instance.tick(dt);
+    });
+
+    this.prevTimestamp = timestamp;
+    HydraSync.window.requestAnimationFrame(this.step);
+  }
+  register(hydraInstance: any) {
+    this.hydraInstances.push(hydraInstance);
+  }
+}
+
+const useHydraSync = (window?: Window) => {
+  if (window) {
+    HydraSync.window = window;
+  }
+  return new HydraSync();
+};
+
+export default useHydraSync;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,11 @@ const Home: NextPage = () => {
     ssr: false,
   });
 
-  console.log("rendered");
+  const MultiCanvas = dynamic(() => import("../components/Canvas2"), {
+    ssr: false,
+  });
+
+  const templates = [".osc(10, 0.1, 0.5).out()", ".osc(20, 0.1, 1).out()"];
 
   return (
     <div className="flex h-full w-screen">
@@ -14,10 +18,20 @@ const Home: NextPage = () => {
         id="feed"
         className="mx-auto flex flex-col p-4 w-1/2 shrink-0 bg-slate-800 text-white space-y-4"
       >
-        {/* <p className="w-full text-center text-2xl">visualizer goes here</p> */}
-        {/* <CanvasPreview /> */}
         <p className="text-2xl pl-2">Multiple Instances of Hydra</p>
-        <MultiCanvasNonSSR />
+        {/* <MultiCanvasNonSSR /> */}
+        <MultiCanvas
+          detectAudio={false}
+          canvasNo={0}
+          makeGlobal={false}
+          hydraFunc={".osc(10, 0.1, 0.5).out()"}
+        />
+        <MultiCanvas
+          detectAudio={false}
+          canvasNo={1}
+          makeGlobal={false}
+          hydraFunc={".osc(20, 0.1, 1).out()"}
+        />
       </div>
     </div>
   );
