76ae38961beb02a1c3b5720f6e5a9a78414bc6f5
Dean Wallflower <dean@deanwallflower.com>
2022-08-17 12:34:44 +0200

    map over JSON to produce hydra functions as strings

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,34 +1,36 @@
-import { useEffect } from "react";
-import dynamic from "next/dynamic";
 const Hydra = require("hydra-synth");
 import useHydraSync from "../hooks/useHydraSync";
 
 interface CanvasProps {
   detectAudio: boolean;
   canvasNo: number;
-  makeGlobal: boolean;
   hydraFunc: string;
+  width: number;
+  height: number;
 }
 
 const Canvas = ({
   detectAudio,
   canvasNo,
-  makeGlobal = false,
   hydraFunc,
+  width,
+  height,
 }: CanvasProps) => {
   const hydraSync = useHydraSync(window);
   const createHydra = () => {
     // create a new canvas and append it to the feed
     const feed = document.getElementById("feed");
     const canvas = document.createElement("canvas");
-    canvas.id = `canvas${canvasNo}`;
+    canvas.id = `canvas-${canvasNo}`;
+    canvas.width = width;
+    canvas.height = height;
     feed?.appendChild(canvas);
 
     // instanciate a new hydra
     const hydra = new Hydra({
       detectAudio: detectAudio,
       canvas: document.getElementById(canvas.id),
-      makeGlobal: makeGlobal,
+      makeGlobal: false,
       autoLoop: false,
     }).synth;
     hydraSync.register(hydra);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,14 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import TemplateExample from "../data/TemplateExample.json";
 
-const templates = [
-  ".osc(10, 0.1, 0.5).out()",
-  ".osc(20, 0.1, 1).out()",
-  ".osc(10, 0.1, 0.5).out()",
-  ".osc(20, 0.1, 1).out()",
-  ".osc(10, 0.1, 0.5).out()",
-  ".osc(20, 0.1, 1).out()",
-  ".osc(10, 0.1, 0.5).out()",
-  ".osc(20, 0.1, 1).out()",
-  ".osc(10, 0.1, 0.5).out()",
-  ".osc(20, 0.1, 1).out()",
-  ".osc(10, 0.1, 0.5).out()",
-  ".osc(20, 0.1, 1).out()",
-  ".osc(10, 0.1, 0.5).out()",
-  ".osc(20, 0.1, 1).out()",
-  ".osc(10, 0.1, 0.5).out()",
-  ".osc(20, 0.1, 1).out()",
-];
+const HydraFunctionsArray = TemplateExample[0].templates.map((template) => {
+  return `.${template.name}(${template.inputs.map((input) => {
+    return `${input.value === null ? input.default : input.value}`;
+  })}).out()`;
+});
+
+console.log(HydraFunctionsArray);
 
 const Home: NextPage = () => {
   const CanvasNonSSR = dynamic(() => import("../components/Canvas"), {
@@ -33,14 +22,15 @@ const Home: NextPage = () => {
         className="mx-auto flex flex-col p-4 w-1/2 shrink-0 bg-slate-800 text-white space-y-4"
       >
         <p className="text-2xl pl-2">Multiple Instances of Hydra</p>
-        {templates.map((template, i) => {
+        {HydraFunctionsArray.map((HydraFunction, i) => {
           return (
             <CanvasNonSSR
               key={i}
               detectAudio={false}
               canvasNo={i}
-              makeGlobal={false}
-              hydraFunc={template}
+              hydraFunc={HydraFunction}
+              width={100}
+              height={(100 * 9) / 16}
             />
           );
         })}
